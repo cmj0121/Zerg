d4d19a06844d27e614c9a72a7a168974edddf752
cmj <cmj@cmj.tw>
2017-07-22 12:28:41 +0800

    Always PROLOGUE and EPILOGUE when emit AST node
    	Should always PROLOGUE and EPILOGUE when compile a AST
    	since it would contains local variables in AST

--- src/zerg/zerg.cpp
+++ src/zerg/zerg.cpp
@@ -25,11 +25,34 @@ void Zerg::compile(std::string src) {
 			//this->parser(BUILTIN_LIBRARY);
 		}
 
-		this->emit(IR_LABEL, ZASM_MAIN_FUNCTION);
 		node = Parser::parser(src);
-		this->emitIR(node);
+		this->emit(node, ZASM_MAIN_FUNCTION);
 		this->emit(IR_CONDITION_RET);
 	}
+}
+
+void Zerg::emit(AST *node, std::string name) {
+	int cnt = 0;
+	char buff[BUFSIZ] = {0};
+
+	/* caculate the number of local variables */
+	if (ZTYPE_UNKNOWN == node->type()) {
+		for (int i = 0; i < node->length(); ++i)
+			switch(node->child(i)->type()) {
+				case ZTYPE_RASSIGN:
+				case ZTYPE_LASSIGN:
+					cnt ++;
+					break;
+				default:
+					break;
+			}
+	}
+	snprintf(buff, sizeof(buff), "0x%X", cnt*PARAM_SIZE);
+
+	this->emit(IR_LABEL, ZASM_MAIN_FUNCTION);
+	this->emit(IR_PROLOGUE, buff);
+	this->emitIR(node);
+	this->emit(IR_EPILOGUE, buff);
 }
 
 void Zerg::emit(IROP opcode, std::string dst, std::string src, std::string size) {
